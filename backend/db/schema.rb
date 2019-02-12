52ba87a5184896186b851676e90270d5c700f300
Tauqeer <tauqeer.ahmad2008@gmail.com>
2019-02-12 20:08:56 +0500

    Answers: Created answers model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_150630) do
+ActiveRecord::Schema.define(version: 2019_02_12_150756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "content"
+    t.bigint "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "isbn", limit: 17, null: false
@@ -29,4 +37,5 @@ ActiveRecord::Schema.define(version: 2019_02_12_150630) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
